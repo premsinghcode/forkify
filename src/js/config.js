93041fdc76f8deac2b70179ba3e0b08fe586c49e
premsinghcode <premsinghcode@gmail.com>
2022-01-23 17:51:30 +0530

    completed app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '52d0b1a8-88eb-488b-842c-a7cc71da496a';
+export const MODAL_CLOSE_SEC = 2.5;
